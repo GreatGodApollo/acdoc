0b58d28b485cd228b3e04daf86f1eca250c6fd64
324Luke <me@lukewhrit.xyz>
2020-04-01 00:23:08 -0400

    Update head object

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,16 @@
 module.exports = {
   title: 'Apollo Core',
+  description: 'Bukkit plugin framework',
   head: [
-    ['link', { rel: 'icon', href: '/logo.png' }],
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon.png' }],
     ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
-    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/mstile-150x150.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#2b5797' }]
   ],
   plugins: [
     [
